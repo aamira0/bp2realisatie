1fac9b3e7080d83e60448a6ba0892804832778da
aamira0 <150680980+aamira0@users.noreply.github.com>
2024-05-28 13:20:33 +0200

    Onnodige code
    -Onnodige code verwijdert, Ik gebruik voortgang niet

--- src/main/java/com/example/bp2realisatie/classes/Doel.java
+++ src/main/java/com/example/bp2realisatie/classes/Doel.java
@@ -1,22 +1,8 @@
 package com.example.bp2realisatie.classes;
 
 public class Doel extends Financieel{
-    private double voortgang;
 
     public Doel(String naam, double bedrag) {
         super(naam, bedrag);
-        this.voortgang = 0.0;
-    }
-
-    public double getVoortgang() {
-        return voortgang;
-    }
-
-    public void updateVoortgang(double bedrag) {
-        voortgang += bedrag;
-    }
-
-    public boolean isDoelBereikt() {
-        return voortgang >= getBedrag();
     }
 }
